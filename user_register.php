edeb91ed6fa176633cd7ec39394af690332d2101
r.shigaki <ryusei_shigaki@en-konkatsu.com>
2020-01-09 01:57:00 +0900

    user_register変更

--- user_register.php
+++ user_register.php
@@ -1,9 +1,11 @@
 <?php
 	session_start();
 
-	$dsn = "pgsql:host=ec2-54-163-234-44.compute-1.amazonaws.com;port=5432;dbname=dc7o0q71ppj44d;user=hocrgjjexhgqex;password=5e9482b0b333e810d2dda163ed524eed0de4a376f7a1bca74160378d5a59aca8";
+	$dsn = "pgsql:host=ec2-54-163-234-44.compute-1.amazonaws.com;port=5432;dbname=dc7o0q71ppj44d;";
+	$user_name = "hocrgjjexhgqex";
+	$password = "5e9482b0b333e810d2dda163ed524eed0de4a376f7a1bca74160378d5a59aca8";
 	try{
-        $pdo = new PDO($dsn,'','',
+        $pdo = new PDO($dsn,$user_name,$password,
             [
                 PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
                 PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
