67cb593008f20475edb6b6048712a5bf09850cc3
r.shigaki <ryusei_shigaki@en-konkatsu.com>
2020-01-09 04:29:04 +0900

    add map

--- map_user.js
+++ map_user.js
@@ -1,5 +1,9 @@
 let map;
 
+$(document).ready(function() {
+    initMap();
+});
+
 function initMap() {
     map = new google.maps.Map(document.getElementById('map'), {
         center: {lat: -34.397, lng: 150.644},
