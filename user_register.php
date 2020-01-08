e4432f55538e3124bcc978ace01ce8a4bdba9e39
r.shigaki <ryusei_shigaki@en-konkatsu.com>
2020-01-09 01:54:22 +0900

    user_register変更

--- user_register.php
+++ user_register.php
@@ -3,7 +3,7 @@
 
 	$dsn = "pgsql:host=ec2-54-163-234-44.compute-1.amazonaws.com;port=5432;dbname=dc7o0q71ppj44d;user=hocrgjjexhgqex;password=5e9482b0b333e810d2dda163ed524eed0de4a376f7a1bca74160378d5a59aca8";
 	try{
-        $pdo = new PDO($dsn,
+        $pdo = new PDO($dsn,'','',
             [
                 PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
                 PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
@@ -11,19 +11,6 @@
         );
 	} catch (PDOException $e) {
 		header('Content-Type: text/plain; charset=UTF-8', true, 500);
-		?>
-		<html>
-			<body>
-            エラーが発生しました。<br />
-                <?php echo $e; ?>
-			</body>
-		</html>
-        <?php
+		echo $e;
 	}
-	?>
-<html>
-    <body>
-        表示成功！
-    </body>
-</html>
-
+	echo "接続成功！";
