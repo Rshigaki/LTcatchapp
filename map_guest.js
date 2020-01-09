1f4dc282fbd1c2efd692f25492befd5e2c184bbf
r.shigaki <ryusei_shigaki@en-konkatsu.com>
2020-01-10 08:48:04 +0900

    add Map

--- map_guest.js
+++ map_guest.js
@@ -9,6 +9,16 @@ let success = (pos) => {
         center: currentPosition,
         zoom: 14
     });
+    new google.maps.Circle({
+        center: currentPosition,       // 中心点(google.maps.LatLng)
+        fillColor: '#ff0000',   // 塗りつぶし色
+        fillOpacity: 0.5,       // 塗りつぶし透過度（0: 透明 ⇔ 1:不透明）
+        map: map,             // 表示させる地図（google.maps.Map）
+        radius: 95330,          // 半径（ｍ）
+        strokeColor: '#ff0000', // 外周色
+        strokeOpacity: 1,       // 外周透過度（0: 透明 ⇔ 1:不透明）
+        strokeWeight: 1         // 外周太さ（ピクセル）
+    });
     nearbysearch(currentPosition);
 };
 
