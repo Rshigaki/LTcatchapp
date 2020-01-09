755eed1de45aa9eade039f565321b7938a00b2ae
r.shigaki <ryusei_shigaki@en-konkatsu.com>
2020-01-09 17:42:49 +0900

    add Map

--- map_guest.js
+++ map_guest.js
@@ -1,15 +1,17 @@
 let map;
+let currentPosition;
 
 let success = (pos) => {
-    let currentPosition = new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude);
+    currentPosition = new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude);
     map = new google.maps.Map(document.getElementById('map'), {
         center: currentPosition,
         zoom: 15
     });
+    nearbysearch(currentPosition);
 };
 
 let error = (err) => {
-    msg = 'エラーが発生しました: ' + err;
+    let msg = 'エラーが発生しました: ' + err;
     console.log(msg);
 };
 
@@ -18,5 +20,55 @@ let options = {
     timeout: 5000,
     maximumAge: 0
 };
+
+function nearbysearch() {
+    var request = {
+        location: currentPosition,
+        radius: '1500',
+        type: ['train_station']
+    };
+
+    service = new google.maps.places.PlacesService(map);
+    service.nearbySearch(request, callback);
+
+    function callback(results, status) {
+        if (status === google.maps.places.PlacesServiceStatus.OK) {
+            //取得したカフェ情報をそれぞれcreateMarkerに入れて、マーカーを作成
+            for (var i = 0; i < results.length; i++) {
+                var place = results[i];
+                //console.log(place)
+                var latlng = place.geometry.location;
+                var icn = place.icon;
+
+                let rendererOptions = {
+                    map: map, // 描画先の地図
+                    draggable: true, // ドラッグ可
+                    preserveViewport: true // centerの座標、ズームレベルで表示
+                };
+                let directionsDisplay = new google.maps.DirectionsRenderer(rendererOptions);
+                let directionsService = new google.maps.DirectionsService();
+                directionsDisplay.setMap(map);
+                let request = {
+                    origin: currentPosition, // スタート地点
+                    destination: latlng, // ゴール地点
+                    travelMode: google.maps.DirectionsTravelMode.WALKING, // 移動手段
+                };
+                directionsService.route(request, function(response,status) {
+                    if (status === google.maps.DirectionsStatus.OK) {
+                        new google.maps.DirectionsRenderer({
+                            map: map,
+                            directions: response,
+                            center: currentPosition,
+                            suppressMarkers: true // デフォルトのマーカーを削除
+                        });
+                        let leg = response.routes[0].legs[0];
+                        makeMarker(leg.end_location, markers.goalMarker, map);
+                    }
+                });
+                createMarker(latlng, icn, place);
+            }
+        }
+    }
+}
 
 navigator.geolocation.getCurrentPosition(success, error, options);
