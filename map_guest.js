e23d4d99b242ac834aea00139846707d5ad1d020
r.shigaki <ryusei_shigaki@en-konkatsu.com>
2020-01-10 09:05:50 +0900

    add Map

--- map_guest.js
+++ map_guest.js
@@ -82,7 +82,7 @@ function nearbysearch() {
                 let rendererOptions = {
                     map: map, // 描画先の地図
                     draggable: true, // ドラッグ可
-                    preserveViewport: true // centerの座標、ズームレベルで表示
+                    preserveViewport: false // centerの座標、ズームレベルで表示
                 };
                 let directionsDisplay = new google.maps.DirectionsRenderer(rendererOptions);
                 let directionsService = new google.maps.DirectionsService();
