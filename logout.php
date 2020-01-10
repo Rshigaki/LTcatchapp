929f0033aed71873834ca61a328c0fab02e75574
r.shigaki <ryusei_shigaki@en-konkatsu.com>
2020-01-10 10:40:51 +0900

    add Map

--- logout.php
+++ logout.php
@@ -1,3 +1,7 @@
 <?php
-	session_destroy();
+	session_start();
+	unset($_SESSION["NAME"]);
+	unset($_SESSION["PASSWORD"]);
+	unset($_SESSION["HOME_STATION"]);
+	unset($_SESSION["LOGGED_IN"]);
 	header("location: ./logouted.php");
